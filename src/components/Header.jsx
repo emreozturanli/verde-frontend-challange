a89c48027eb6f9791363bdff14b82dafc16c07e7
emre <emreozturanli1@gmail.com>
2022-08-18 00:19:37 +0300

    header started

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { IoMdNotifications } from 'react-icons/io';
+import { AiTwotoneAppstore } from 'react-icons/ai';
+import profile from "../assets/profile_p.jpg"
 
 const Header = () => {
   return (
-    <div className='text-3xl font-bold underline bg-white'>Header</div>
+    <header className='bg-white container shadow-md m-auto mt-8 p-5 flex justify-between items-center'>
+      <h1 className='text-3xl font-bold'>Arbit Blog</h1>
+      <nav className='flex gap-4 items-center'>
+        <h3 className='text-2xl font-semibold'>Posts</h3>
+        <IoMdNotifications size={25}/>
+        <AiTwotoneAppstore size={25}/>
+        <img className='rounded-full w-10 h-10' src={profile} alt="profile" />
+      </nav>
+    </header>
   )
 }
 
